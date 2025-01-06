4a2d6587fe853033db6f2937a417a3bca96f6500
barneywill <jarodwill@163.com>
2025-01-06 21:20:43 +0100

    update deequ

--- Deequ/README.md
+++ Deequ/README.md
@@ -7,4 +7,24 @@ Deequ is a library built on top of Apache Spark for defining "unit tests for dat
 #### Put it under $SPARK_HOME/jars
 https://repo1.maven.org/maven2/com/amazon/deequ/deequ/2.0.8-spark-3.5/deequ-2.0.8-spark-3.5.jar
 
-## 2 Example
+## 2 Scenario
+
+### 2.1 Data Profiling
+Deequ supports single-column profiling of such data and its implementation scales to large datasets with billions of rows. 
+- completeness
+- approximateNumDistinctValues
+- dataType
+
+### 2.2 Anomaly Detection
+Check the change of the size of a dataset every day to ensure it does not change drastically.
+- the number of rows on a given day should not be more than double of what we have seen on the day before.
+
+### 2.3 Automatic Suggestion of Constraints
+Automatic constraint suggestion first profiles the data and then applies a set of heuristic rules to suggest constraints.
+- description: 'valuable' has less than 62% missing values
+- codeForConstraint: .hasCompleteness("valuable", _ >= 0.38, Some("It should be above 0.38!"))
+
+### 2.4 Storing Computed Metrics in a MetricsRepository
+Deequ allows us to persist the metrics we computed on dataframes in a so-called MetricsRepository. 
+- the ability to save the result and query it like a DataFrame
+
---
+++ Deequ/deequ_spark.scala
@@ -0,0 +1,48 @@
+import org.apache.spark._
+import org.apache.spark.rdd._
+import com.amazon.deequ.VerificationSuite
+import com.amazon.deequ.checks.{Check, CheckLevel, CheckStatus}
+import com.amazon.deequ.constraints.ConstraintStatus
+
+object WordCountRDD {
+    def main(args : Array[String]) : Unit => {
+        val sparkConf = new SparkConf()
+            .setMaster("local[*]")
+            .setAppName("Deequ_Scala")
+        val sc = SparkContext(sparkConf)
+
+        case class Item(id: Long, productName: String, description: String, priority: String, numViews: Long)
+
+        val rdd = spark.sparkContext.parallelize(Seq(
+            Item(1, "Thingy A", "awesome thing.", "high", 0),
+            Item(2, "Thingy B", "available at http://thingb.com", null, 0),
+            Item(3, null, null, "low", 5),
+            Item(4, "Thingy D", "checkout https://thingd.ca", "low", 10),
+            Item(5, "Thingy E", null, "high", 12)))
+
+        val data = spark.createDataFrame(rdd)
+
+        val verificationResult = VerificationSuite()
+            .onData(data)
+            .addCheck(Check(CheckLevel.Error, "unit testing my data")
+                .hasSize(_ == 5) // we expect 5 rows
+                .isComplete("id") // should never be NULL
+                .isUnique("id") // should not contain duplicates
+                .isComplete("productName") // should never be NULL
+                .isContainedIn("priority", Array("high", "low")) // should only contain the values "high" and "low"
+                .isNonNegative("numViews") // should not contain negative values
+                .containsURL("description", _ >= 0.5) // at least half of the descriptions should contain a url
+                .hasApproxQuantile("numViews", 0.5, _ <= 10)) // half of the items should have less than 10 views
+            .run()
+        
+        if (verificationResult.status == CheckStatus.Success) {
+            println("The data passed the test, everything is fine!")
+        } else {
+            println("We found errors in the data:\n")
+            val resultsForAllConstraints = verificationResult.checkResults.flatMap { case (_, checkResult) => checkResult.constraintResults }
+            resultsForAllConstraints.filter { _.status != ConstraintStatus.Success }
+                .foreach { result => println(s"${result.constraint}: ${result.message.get}") }
+        }
+    }
+}
+
